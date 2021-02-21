7218217b9703b6e3ce6bbe6df4b29da27b28ec15
conconartist <conconart.info@gmail.com>
2021-02-21 12:32:34 -0700

    Set up network request to API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import Quotes from './Quotes/Quotes.js'
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      quotes: []
+      quote: "", 
+      error: false
     }
+  }
+  componentDidMount = () => {
+    fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({quote: data})
+      console.log(this.state.quote)
+    }).catch(error => {
+      this.setState({error: true})
+    })
   }
   render() {
     return (
       <main className='App'>
-        <h1>Swanson Says</h1>
-        <Quotes />  
+        <h1>Swanson Says, "{this.state.quote}"</h1>
+        <button>Say Something Else</button>
       </main>
     )
   }
